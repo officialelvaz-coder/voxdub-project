e46d992a409f77619bf1ac7ec2b6a43f2f96e738
officialelvaz-coder <officialelvaz@gmail.com>
2026-03-17 02:24:11 +0400

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,13 @@
 import { Toaster } from './components/ui/sonner';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
+import { AuthProvider } from './context/AuthContext';
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
       <Toaster position="top-center" />
-    </>
+    </AuthProvider>
   );
 }
