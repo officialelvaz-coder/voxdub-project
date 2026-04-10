98f22757d0fb712b2bed7ee968c6be1fb0ff362c
officialelvaz-coder <officialelvaz@gmail.com>
2026-04-10 20:38:00 +0400

    firebase.ts

--- src/app/components/firebase.ts
+++ src/app/components/firebase.ts
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDyOoLEsDzhQdcqfDgntBejE-xTnLULbTg",
   authDomain: "voxdub-a541c.firebaseapp.com",
@@ -16,6 +11,8 @@ const firebaseConfig = {
   measurementId: "G-R2EBWHN9S2"
 };
 
-// Initialize Firebase
+// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// تصدير قاعدة البيانات لاستخدامها في الملفات الأخرى
+export const db = getFirestore(app);
