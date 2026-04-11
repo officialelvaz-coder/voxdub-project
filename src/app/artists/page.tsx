85224c60b0a656ce72f3a6f02b1f988e3d055e1c
officialelvaz-coder <officialelvaz@gmail.com>
2026-04-11 14:37:49 +0400

    Update page.tsx

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../../components/firebase';
+import { db } from '@/lib/firebase';
 
 interface Artist {
   id: string;
